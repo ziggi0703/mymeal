39f68105f1dbe59075f1e233a141d3a1a74ced6b
Michael Ziegler <ziggi_0@web.de>
2014-10-04 16:11:16 +0200

    Change db engine to psotgresql on local host

--- mymeal/settings.py
+++ mymeal/settings.py
@@ -36,6 +36,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'decision_maker'
 )
 
 MIDDLEWARE_CLASSES = (
@@ -58,8 +59,11 @@ WSGI_APPLICATION = 'mymeal.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'mymeal_db',
+        'USER': 'decisionmaker',
+        'PASSWORD': 'katze123',
+        'HOST': 'localhost'
     }
 }
 
